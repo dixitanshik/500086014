4ba658c525c987ea2f9109cd3990ec388de0d086
dixitanshik <dixitanshika111@gmail.com>
2023-09-10 10:08:01 +0530

    question 1

---
+++ Question1/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './HelloWorld'; // Assuming this file is in the same directory
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
